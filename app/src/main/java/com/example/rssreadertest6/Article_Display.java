3c235624aa978826efe8516afa1384e2c696554d
C00282704 <C00282704@setu.ie>
2026-02-09 16:13:55 +0000

    TTS works, going to work on multThreading.

--- app/src/main/java/com/example/rssreadertest6/Article_Display.java
+++ app/src/main/java/com/example/rssreadertest6/Article_Display.java
@@ -5,8 +5,13 @@ import static com.example.rssreadertest6.R.id.textToSpeechButton;
 import static java.util.Locale.ENGLISH;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
@@ -16,6 +21,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,11 +29,17 @@ import androidx.core.view.WindowInsetsCompat;
 public class Article_Display extends AppCompatActivity {
 
     TextToSpeech tts;
+    Boolean ttsPressed = false;
+    ImageButton ttsButton;
+    TextView contentView;
+    WebView article;
+    boolean ttsDone;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent receiverIntent = getIntent();
         String uri = receiverIntent.getStringExtra("uri");
-        System.out.println(uri);
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -46,10 +58,27 @@ public class Article_Display extends AppCompatActivity {
                 }
             }
         });
+        tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onDone(String s) {
+                ttsDone = true;
+            }
+
+            @Override
+            public void onError(String s) {
+
+            }
+
+            @Override
+            public void onStart(String s) {
+                ttsDone = false;
+            }
+        });
 
 
 
-        WebView article = findViewById(R.id.WebView);
+        article = findViewById(R.id.WebView);
+        article.loadUrl(uri);
 
         ImageButton iButton = findViewById(R.id.returnButton);
         iButton.setOnClickListener(new View.OnClickListener(){
@@ -57,12 +86,13 @@ public class Article_Display extends AppCompatActivity {
             public void onClick(View view) {
                 Intent senderIntent = new Intent(Article_Display.this, MainActivity.class);
                 finish();
+                tts.stop();
+                tts.shutdown();
                 startActivity(senderIntent);
             }
         });
-        TextView contentView = (TextView) findViewById(R.id.contentView);
+        contentView = (TextView) findViewById(R.id.contentView);
         class MyJavaScriptInterface {
-            private TextView contentView;
             public MyJavaScriptInterface(TextView aContentView) {
                 contentView = aContentView;
             }
@@ -86,19 +116,45 @@ public class Article_Display extends AppCompatActivity {
                 view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0].innerText);");
             }
         });
+        ttsButton = findViewById(textToSpeechButton);
+        ttsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (ttsPressed == false) {
+                    ttsButton.setImageResource(android.R.drawable.ic_lock_silent_mode_off);
+                    ttsPressed = true;
+                    String text = contentView.getText().toString();
+                    runTTS(text);
 
-        article.loadUrl(uri);
-        ImageButton ttsButton = findViewById(textToSpeechButton);
-//        ttsButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-////                String text = "XXXXXXXXXXXXXXX";
-//                WebView element = driver.findElement(By.id("exampleId"));
-//                String text = element.getText();
-//
-//                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-//                //FInd out how to get the text off of the article
-//            }
-//        });
+                }
+                else {
+                    ttsButton.setImageResource(android.R.drawable.ic_lock_silent_mode);
+                    ttsPressed = false;
+                    tts.stop();
+                }
+            }
+        });
+    }
+    public void runTTS(String text){
+        if(text.length() > 400){
+            String[] strs;
+            int divisions = 0;
+            int length = text.length();
+            int start = 0;
+            while(length > 400){
+                length = length/2;
+                divisions = divisions + 1;
+            }
+            for(int i = 0; i <= divisions; i++) {
+                tts.speak(text.substring(start, (start + 400)), TextToSpeech.QUEUE_FLUSH, null, "Article_ID");
+                while(!ttsDone){
+
+                }
+                start = start + 400;
+            }
+        }
+        else{
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "Article_ID");
+        }
     }
 }
