1c2885df8645a220e0b9ee30a75d947393efe7c3
C00282704 <C00282704@setu.ie>
2026-07-17 16:55:57 +0100

    Should be mostly fine. Tried reimplementing Supabase functionality, but its hard and didn't have time.

--- app/src/main/java/com/example/TheiaNewsAggregator/ChangeRSSFeeds.java
+++ app/src/main/java/com/example/TheiaNewsAggregator/ChangeRSSFeeds.java
@@ -23,7 +23,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.json.gson.JsonSyntaxException;
+import com.google.gson.JsonSyntaxException;
 
 // import org.json.JSONArray;
 // import org.json.JSONException;
@@ -83,11 +83,11 @@ public class ChangeRSSFeeds extends AppCompatActivity {
             SharedPreferences prefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE);
             Gson gson = new Gson();
             String jsonString = prefs.getString("RSSFeeds", null);
+            List<String> urls = new ArrayList<>();
             try{
-                List<String> urls = gson.fromJson(jsonString, new TypeToken<List<String>>() {}.getType());
+                urls = gson.fromJson(jsonString, new TypeToken<List<String>>() {}.getType());
             }catch(JsonSyntaxException j){
-                Log.e("JsonSyntaxException", j)
-                List<String> urls = new ArrayList<>()
+                urls = new ArrayList<>();
             }
 
             if(urls == null || urls.isEmpty()){
@@ -109,6 +109,19 @@ public class ChangeRSSFeeds extends AppCompatActivity {
                 List<String> selectedUrls = (newJString != null)
                 ? gson.fromJson(newJString, new TypeToken<List<String>>(){}.getType())
                 : new ArrayList<>();
+
+                if (newJString != null) {
+                    try{
+                        selectedUrls = gson.fromJson(newJString, new TypeToken<List<String>>() {}.getType());
+                    }catch(JsonSyntaxException j){
+                        selectedUrls = new ArrayList<>();
+                    }
+                } else {
+                    selectedUrls = new ArrayList<String>();
+                }
+
+                //supabaseFeeds();
+
                 runOnUiThread(() -> {
                     ImageButton addButton = findViewById(R.id.addFeedButton);
                     addButton.setOnClickListener(new View.OnClickListener() {
@@ -120,13 +133,14 @@ public class ChangeRSSFeeds extends AppCompatActivity {
                                 .setPositiveButton("Add", (dialog, which) -> {
                                         String userText = input.getText().toString();
                                         if (!userText.isEmpty() && isValidUrlSyntax(userText)) {
-                                            SharedPreferences prefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE);
-                                            Gson gson = new Gson();
-
                                             String jsonString = prefs.getString("RSSFeeds", null);
                                             List<String> feeds;
                                             if (jsonString != null) {
-                                                feeds = gson.fromJson(jsonString, new TypeToken<List<String>>() {}.getType());
+                                                try{
+                                                    feeds = gson.fromJson(jsonString, new TypeToken<List<String>>() {}.getType());
+                                                }catch(JsonSyntaxException j){
+                                                    feeds = new ArrayList<>();
+                                                }
                                             } else {
                                                 feeds = new ArrayList<String>();
                                             }
@@ -169,6 +183,7 @@ public class ChangeRSSFeeds extends AppCompatActivity {
                         for(int i2 = 0; i2 < selectedUrls.size(); i2++){
                             if(selectedUrls.get(i2).equals(finalUrls.get(i))){
                                 newCB.setChecked(true);
+                                break;
                             }
                         }
 
@@ -197,5 +212,91 @@ public class ChangeRSSFeeds extends AppCompatActivity {
             return false;
         }
     }
+    // public void supabaseFeeds(){
+        
+    //         String url = "https://zganowuduwhsgxdhlxcl.supabase.co/rest/v1/RSSFeeds";
+    //         String key = "sb_publishable_vSfZh1SKFxeBtuTdmErOSQ_hI7Ml1rU";
+    //         OkHttpClient client = new OkHttpClient();
+
+    //         Request request = new Request.Builder().url(url).addHeader("apikey", key).addHeader("Authorization", "Bearer " + key).build();
+
+    //         try (Response response = client.newCall(request).execute()) {
+    //             String responseString = "";
+    //             if (response.body() != null) {
+    //                 responseString = response.body().string();
+    //             }
+
+    //             JSONArray jsonArray = new JSONArray(responseString);
+
+    //             // Loop through each row
+    //             for (int i = 0; i < jsonArray.length(); i++) {//Get the name for each Site for the CheckBox Names
+    //                 JSONObject row = jsonArray.getJSONObject(i);
+    //                 if (!row.getString("name").isEmpty()) {
+    //                     String feedName = row.getString("name");
+    //                     names.add(feedName);
+    //                     String link = row.getString("link");
+    //                     links.add(link);
+    //                 }
+    //             }
+
+    //             for (int i = 0; i < jsonArray.length(); i++) {//Get the logo for each Site for the CheckBox Names
+    //                 JSONObject row = jsonArray.getJSONObject(i);
+    //                 if (!row.getString("logoName").isEmpty()) {
+    //                     String logoName = row.getString("logoName");
+    //                     String imageUrl = "https://zganowuduwhsgxdhlxcl.supabase.co/storage/v1/object/public/FeedImages/" + logoName;
+    //                     Request imageNameRequest = new Request.Builder().url(imageUrl).addHeader("apikey", key).addHeader("Authorization", "Bearer " + key).build();
+
+    //                     try (Response imageResponse = client.newCall(imageNameRequest).execute()) {
+    //                         if (imageResponse.body() != null) {
+    //                             byte[] imageBytes = imageResponse.body().bytes();
+    //                             Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    //                             images.add(bitmap);
+    //                         }
+    //                     } catch (Exception e) {
+    //                         e.printStackTrace();
+    //                     }
+    //                 }
+    //             }
+    //         } catch (IOException e) {
+    //             throw new RuntimeException(e);
+    //         } catch (JSONException e) {
+    //             throw new RuntimeException(e);
+    //         }
+    //         if (names != null) {
+    //             runOnUiThread(() -> {
+    //                 LinearLayout mainContainer = findViewById(R.id.LLMain);
+    //                 for (int i = 0; i < names.size(); i++) {
+    //                     Log.i("LOG", "2-Printing");
+    //                     //Create button
+    //                     LinearLayout newLayout = new LinearLayout(ChangeRSSFeeds.this);
+    //                     CheckBox newCB = new CheckBox(ChangeRSSFeeds.this);
+    //                     newCB.setText(names.get(i));//RSS Feed Name
+    //                     String link = links.get(i);
+    //                     int index = i;
+    //                     newCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    //                         @Override
+    //                         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    //                             if (isChecked) {
+    //                                 prefLinks.add(links.get(index));
+    //                             } else {
+    //                                 prefLinks.remove(links.get(index));
+    //                             }
+    //                         }
+    //                     });
+
+    //                     ImageView newLogo = new ImageView(ChangeRSSFeeds.this);
+    //                     newLogo.setImageBitmap(images.get(i));
+
+    //                     LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(205,350);
+    //                     newCB.setLayoutParams(lp);
+    //                     newLogo.setLayoutParams(lp);
+
+    //                     newLayout.addView(newCB);
+    //                     newLayout.addView(newLogo);
+    //                     mainContainer.addView(newLayout);
+    //                 }
+    //             });
+    //         }
+    // }
 
 }
